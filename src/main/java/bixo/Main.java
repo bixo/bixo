0d2da4bf9af19bd5b02fe2955b13e3b92da0c738
Stefan Groschupf <sg@101tec.com>
2009-03-31 19:11:54 -0700

    BIXO-1, ivy based build and simple project skeleton.

--- src/main/java/bixo/Main.java
+++ src/main/java/bixo/Main.java
@@ -2,52 +2,18 @@
  * Copyright (c) 2009 The Bixo Project. All rights reserved
  */
 
-
 package bixo;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  *
  */
-public class Main
-  {
-  private static final Logger LOG = LoggerFactory.getLogger( Main.class );
+public class Main {
 
-  private Options options;
-
-  public static void main( String[] args ) throws URISyntaxException, IOException
-    {
-    long start = System.currentTimeMillis();
-
-    // env: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-    Options options = new Options();
-    CmdLineParser parser = new CmdLineParser( options );
-
-    try
-      {
-      parser.parseArgument( args );
-      }
-    catch( CmdLineException e )
-      {
-      System.err.println( e.getMessage() );
-      printUsageAndExit( parser );
-      }
-
-    }
-
-
-  private static void printUsageAndExit( CmdLineParser parser )
-    {
-    parser.printUsage( System.err );
-
-    System.exit( -1 );
-    }
-
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    System.out.println("Hello World!");
   }
+
+}
---
+++ src/test/java/bixo/TestMain.java
@@ -0,0 +1,10 @@
+package bixo;
+
+import org.junit.Test;
+
+public class TestMain {
+  @Test
+  public void testMain() throws Exception {
+    Main.main(new String[0]);
+  }
+}
