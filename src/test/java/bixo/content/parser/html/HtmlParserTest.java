af6708e290e16d3c9ceaf60d0cde9676fe93d8ca
Stefan Groschupf <sg@101tec.com>
2009-04-29 16:00:43 -0700

    fixing HTMLParserTest.

--- src/test/java/bixo/content/parser/html/HtmlParserTest.java
+++ src/test/java/bixo/content/parser/html/HtmlParserTest.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.io.BytesWritable;
 import org.junit.Assert;
 import org.junit.Test;
 
-import bixo.datum.FetchContentTuple;
+import bixo.datum.FetchStatusCode;
+import bixo.datum.FetchedDatum;
 import bixo.datum.Outlink;
 import bixo.datum.ParsedDatum;
 import bixo.parser.IParse;
@@ -20,50 +22,49 @@ import bixo.parser.ParseResult;
 import bixo.parser.html.HtmlParser;
 import bixo.parser.html.HtmlParserFactory;
 
-
 public class HtmlParserTest {
-    
-    private FetchContentTuple makeContentTuple(URL path) throws IOException {
+
+    private FetchedDatum makeFetchedDat(URL path) throws IOException {
         File file = new File(path.getFile());
-        byte[] bytes = new byte[(int)file.length()];
+        byte[] bytes = new byte[(int) file.length()];
         DataInputStream in = new DataInputStream(new FileInputStream(file));
         in.readFully(bytes);
-        
-        FetchContentTuple content = new FetchContentTuple(path.toExternalForm(), path.toExternalForm(),
-                        System.currentTimeMillis(), bytes, "text/html", 0);
 
-        return content;
+        FetchedDatum fetchedDatum = new FetchedDatum(FetchStatusCode.FETCHED, path.toExternalForm().toString(), path.toExternalForm().toString(), System.currentTimeMillis(), new BytesWritable(bytes),
+                        "text/html", 0, null);
+
+        return fetchedDatum;
     }
-    
+
     @Test
     public void testHtlmParsing() throws IOException {
         URL path = HtmlParserTest.class.getResource("/" + "simple-page.html");
 
         HtmlParser parser = new HtmlParser();
-        FetchContentTuple content = makeContentTuple(path);
+        FetchedDatum content = makeFetchedDat(path);
         ParseResult parse = parser.getParse(content);
-        
+
         IParse p = parse.get(path.toExternalForm());
         Assert.assertNotNull(p);
-        
+
         File parsedTextFile = new File(HtmlParserTest.class.getResource("/" + "simple-page.txt").getFile());
         Assert.assertEquals(FileUtils.readFileToString(parsedTextFile, "utf-8"), p.getText());
-        
+
         Outlink[] outlinks = p.getData().getOutlinks();
         Assert.assertEquals(10, outlinks.length);
     }
-    
+
     @Test
     public void testIParserInterface() throws IOException {
         IParserFactory factory = new HtmlParserFactory();
         IParser parser = factory.newParser();
-        
+
         URL path = HtmlParserTest.class.getResource("/" + "simple-page.html");
-        FetchContentTuple content = makeContentTuple(path);
-        
+        FetchedDatum content = makeFetchedDat(path);
+
         ParsedDatum result = parser.parse(content);
         File parsedTextFile = new File(HtmlParserTest.class.getResource("/" + "simple-page.txt").getFile());
-        Assert.assertEquals(FileUtils.readFileToString(parsedTextFile, "utf-8"), result.getText());
+        Assert.assertEquals(FileUtils.readFileToString(parsedTextFile, "utf-8"), result.getParsedText());
         Assert.assertEquals(10, result.getOulinks().length);
     }
 }
