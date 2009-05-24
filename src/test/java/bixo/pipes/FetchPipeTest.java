ca8629b3b1b88948a57eba7623b2f0984890eb01
Ken Krugler <kkrugler@transpac.com>
2009-05-24 01:05:11 -0700

    Added test for passing meta-data through the pipe.

--- src/test/java/bixo/pipes/FetchPipeTest.java
+++ src/test/java/bixo/pipes/FetchPipeTest.java
@@ -1,13 +1,17 @@
 package bixo.pipes;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.hadoop.mapred.JobConf;
+import org.junit.Assert;
 import org.junit.Test;
 
 import bixo.cascading.MultiSinkTap;
 import bixo.config.FetcherPolicy;
+import bixo.datum.BaseDatum;
 import bixo.datum.FetchStatusCode;
 import bixo.datum.FetchedDatum;
 import bixo.datum.UrlDatum;
@@ -29,6 +33,8 @@ import cascading.tap.Hfs;
 import cascading.tap.Lfs;
 import cascading.tap.Tap;
 import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
 import cascading.tuple.TupleEntryCollector;
 import cascading.tuple.TupleEntryIterator;
 import cascading.util.Util;
@@ -38,12 +44,19 @@ public class FetchPipeTest extends CascadingTestCase {
     private static final long TEN_DAYS = 1000L * 60 * 60 * 24 * 10;
 
     private Lfs makeInputData(int numDomains, int numPages) throws IOException {
-        Lfs in = new Lfs(new SequenceFile(UrlDatum.FIELDS), "build/test-data/FetchPipeTest/in", true);
+        return makeInputData(numDomains, numPages, null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    private Lfs makeInputData(int numDomains, int numPages, Map<String, Comparable> metaData) throws IOException {
+        Fields sfFields = UrlDatum.FIELDS.append(BaseDatum.makeMetaDataFields(metaData));
+        Lfs in = new Lfs(new SequenceFile(sfFields), "build/test-data/FetchPipeTest/in", true);
         TupleEntryCollector write = in.openForWrite(new JobConf());
         for (int i = 0; i < numDomains; i++) {
             for (int j = 0; j < numPages; j++) {
-                UrlDatum url = new UrlDatum("http://domain-" + i + ".com/page-" + j + ".html?size=10", 0, 0, FetchStatusCode.UNFETCHED, null);
-                write.add(url.toTuple());
+                UrlDatum url = new UrlDatum("http://domain-" + i + ".com/page-" + j + ".html?size=10", 0, 0, FetchStatusCode.UNFETCHED, metaData);
+                Tuple tuple = url.toTuple();
+                write.add(tuple);
             }
         }
         
@@ -105,4 +118,48 @@ public class FetchPipeTest extends CascadingTestCase {
         TupleEntryIterator tupleEntryIterator = validate.openForRead(new JobConf());
         validateLength(tupleEntryIterator, 1);
     }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMetaData() throws Exception {
+        Map<String, Comparable> metaData = new HashMap<String, Comparable>();
+        metaData.put("key", "value");
+        Lfs in = makeInputData(1, 1, metaData);
+
+        Pipe pipe = new Pipe("urlSource");
+        IUrlNormalizer urlNormalizer = new UrlNormalizer();
+        PLDGrouping grouping = new PLDGrouping();
+        LastFetchScoreGenerator scoring = new LastFetchScoreGenerator(System.currentTimeMillis(), TEN_DAYS);
+        IHttpFetcher fetcher = new FakeHttpFetcher(false, 10);
+        FetchPipe fetchPipe = new FetchPipe(pipe, urlNormalizer, grouping, scoring, fetcher, new Fields("key"));
+        
+        String outputPath = "build/test-data/FetchPipeTest/dual";
+        Fields contentFields = FetchedDatum.FIELDS.append(new Fields("key"));
+        Tap status = new Hfs(new TextLine(new Fields(FetchedDatum.BASE_URL_FIELD, FetchedDatum.STATUS_CODE_FIELD), new Fields(FetchedDatum.BASE_URL_FIELD, FetchedDatum.STATUS_CODE_FIELD)), outputPath + "/status", true);
+        Tap content = new Hfs(new SequenceFile(contentFields), outputPath + "/content", true);
+        Tap sink = new MultiSinkTap(status, content);
+
+        // Finally we can run it.
+        FlowConnector flowConnector = new FlowConnector();
+        Flow flow = flowConnector.connect(in, sink, fetchPipe);
+        flow.complete();
+        
+        Lfs validate = new Lfs(new SequenceFile(contentFields), outputPath + "/content");
+        TupleEntryIterator tupleEntryIterator = validate.openForRead(new JobConf());
+        
+        int totalEntries = 0;
+        while (tupleEntryIterator.hasNext()) {
+            TupleEntry entry = tupleEntryIterator.next();
+            totalEntries += 1;
+            
+            Assert.assertEquals(entry.size(), contentFields.size());
+            String metaValue = entry.getString("key");
+            Assert.assertNotNull(metaValue);
+            Assert.assertEquals("value", metaValue);
+        }
+        
+        Assert.assertEquals(1, totalEntries);
+    }
+    
+
 }
