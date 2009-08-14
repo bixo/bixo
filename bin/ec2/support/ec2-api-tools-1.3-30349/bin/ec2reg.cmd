6aa54b38e19a3fe1f2227ac38e839b7b281975c1
Ken Krugler <kkrugler@transpac.com>
2009-08-14 08:10:27 -0700

    More from EC2 script-land

---
+++ bin/ec2/support/ec2-api-tools-1.3-30349/bin/ec2reg.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+setlocal
+
+REM Set intermediate env vars because the %VAR:x=y% notation below
+REM (which replaces the string x with the string y in VAR)
+REM doesn't handle undefined environment variables. This way
+REM we're always dealing with defined variables in those tests.
+set CHK_HOME=_%EC2_HOME%
+
+if "%CHK_HOME:"=%" == "_" goto HOME_MISSING
+
+"%EC2_HOME:"=%\bin\ec2-cmd" RegisterImage %*
+goto DONE
+:HOME_MISSING
+echo EC2_HOME is not set
+exit /b 1
+
+:DONE
