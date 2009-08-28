eac16b4921ab30ff29b06d8b8c0026864581995d
Ken Krugler <kkrugler@transpac.com>
2009-08-28 13:48:12 -0700

    Added tool to extract non-adult DMOZ URLs (vs. domains)

---
+++ src/main/java/bixo/tools/ExtractDmozLinks.java
@@ -0,0 +1,91 @@
+package bixo.tools;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDmozLinks {
+
+    public static void main(String[] args) {
+        // <Topic r:id="Top/Adult/Arts/Animation/Anime/Fan_Works/Fan_Art">
+        Pattern topicPattern = Pattern.compile("[ \t]*<Topic[ \t]+r:id[ \t]*=[ \t]*\"([^\"]+)\">.*");
+        Matcher topicMatcher = topicPattern.matcher("");
+        
+        //   <link r:resource="http://www.geocities.com/kaseychan17/index.html"/>
+        Pattern resourcePattern = Pattern.compile("[ \t]*<link(1|)[ \t]+r:resource[ \t]*=[ \t]*\"([^\"]+)\".*");
+        Matcher resourceMatcher = resourcePattern.matcher("");
+
+        try {
+            String inputFile = args[0];
+            PrintStream outputStream;
+            if (args.length == 2) {
+                outputStream = new PrintStream(new File(args[1]));
+            } else {
+                outputStream = System.out;
+            }
+
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(inputFile), "UTF-8");
+            BufferedReader reader = new BufferedReader(isr);
+            
+            int numLines = 0;
+            int numUrls = 0;
+            
+            boolean inGoodTopic = false;
+            
+            String curLine;
+            String urlString;
+            while ((curLine = reader.readLine()) != null) {
+                numLines += 1;
+                if ((numLines % 100000) == 0) {
+                    System.out.println(String.format(" %d/%d", numLines, numUrls));
+                } else if ((numLines % 1000) == 0) {
+                    System.out.print('.');
+                }
+
+                boolean checkForTopic = !inGoodTopic;
+                if (inGoodTopic) {
+                    if (resourceMatcher.reset(curLine).matches()) {
+                        urlString = resourceMatcher.group(2);
+                        numUrls += 1;
+
+                        try {
+                            URL url = new URL(URLDecoder.decode(urlString, "UTF-8"));
+                            outputStream.println(url.toExternalForm());
+                        } catch (MalformedURLException e) {
+                            System.err.println();
+                            System.err.println("Invalid URL found in dmoz data: " + urlString);
+                            System.err.println("Exception is " + e.getMessage());
+                        } catch (IllegalArgumentException e) {
+                            System.err.println();
+                            System.err.println("Invalid URL found in dmoz data: " + urlString);
+                            System.err.println("Exception is " + e.getMessage());
+                        }
+                    } else {
+                        checkForTopic = true;
+                    }
+                }
+
+                if (checkForTopic && (topicMatcher.reset(curLine).matches())) {
+                    // Skip all adult links...we'll get some, but less this way.
+                    inGoodTopic = !topicMatcher.group(1).contains("/Adult");
+                }
+            }
+            
+            System.out.println("Processed " + numLines + " lines of input");
+            System.out.println("Found " + numUrls + " URLs");
+        } catch (Exception e) {
+            System.err.println("Exception while running tool: " + e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(-1);
+        }
+
+    }
+
+}
