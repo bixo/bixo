b28aac0625990debd4a4c6bb4a9ae261dd55dd4c
Ken Krugler <kkrugler@transpac.com>
2009-10-01 17:20:31 -0700

    Moved test into integration area, as it depends on external (DNS) resources.

---
+++ src/it/java/bixo/fetcher/util/SimpleGroupingKeyGeneratorIntegrationTest.java
@@ -0,0 +1,24 @@
+package bixo.fetcher.util;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import bixo.datum.UrlDatum;
+import bixo.utils.GroupingKey;
+
+public class SimpleGroupingKeyGeneratorIntegrationTest {
+    
+    @Test
+    public void testBogusHostname() throws IOException {
+        SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("user agent");
+        
+        String url = "http://totalbogusdomainxxx.com";
+        UrlDatum urlDatum = new UrlDatum(url);
+        String key = keyGen.getGroupingKey(urlDatum);
+        Assert.assertEquals(GroupingKey.UNKNOWN_HOST_GROUPING_KEY, key);
+    }
+    
+}
