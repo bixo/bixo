1967fe3ad905baad6d4065246333670d51aae454
Ken Krugler <kkrugler@transpac.com>
2009-10-06 17:19:23 -0700

    Added test for robots.txt redirecting to https, and getting an error fetching that.
    Improved test cleanup so that one test failing w/an exception doesn't screw up
    all of the following tests, because the server is still hogging port 8089.

--- src/test/java/bixo/fetcher/util/SimpleGroupingKeyGeneratorTest.java
+++ src/test/java/bixo/fetcher/util/SimpleGroupingKeyGeneratorTest.java
@@ -3,44 +3,64 @@ package bixo.fetcher.util;
 import junit.framework.Assert;
 
 import org.apache.http.HttpStatus;
+import org.junit.After;
 import org.junit.Test;
 import org.mortbay.http.HttpServer;
 
 import bixo.datum.UrlDatum;
 import bixo.fetcher.FixedStatusResponseHandler;
+import bixo.fetcher.RedirectResponseHandler;
 import bixo.fetcher.ResourcesResponseHandler;
 import bixo.fetcher.simulation.SimulationWebServer;
 import bixo.utils.GroupingKey;
 
 public class SimpleGroupingKeyGeneratorTest extends SimulationWebServer {
-
+    
+    private HttpServer _server;
+    
+    @After
+    public void tearDown() throws InterruptedException {
+        if ((_server != null) && _server.isStarted()) {
+            _server.stop();
+        }
+    }
+    
     @Test
     public void testNoRobots() throws Exception {
-        HttpServer server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_NOT_FOUND), 8089);
+        _server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_NOT_FOUND), 8089);
         SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("user agent");
         String url = "http://localhost:8089/page.html";
         UrlDatum urlDatum = new UrlDatum(url);
         String key = keyGen.getGroupingKey(urlDatum);
-        server.stop();
 
-        Assert.assertEquals("127.0.0.1-30000", key);
+        Assert.assertEquals("127.0.0.1-unset", key);
     }
     
     @Test
     public void testDeferredVisit() throws Exception {
-        HttpServer server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_INTERNAL_SERVER_ERROR), 8089);
+        _server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_INTERNAL_SERVER_ERROR), 8089);
         SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("user agent");
         String url = "http://localhost:8089/page.html";
         UrlDatum urlDatum = new UrlDatum(url);
         String key = keyGen.getGroupingKey(urlDatum);
-        server.stop();
+
+        Assert.assertEquals(GroupingKey.DEFERRED_GROUPING_KEY, key);
+    }
+    
+    @Test
+    public void testRobotsHttpsFetchError() throws Exception {
+        _server = startServer(new RedirectResponseHandler("/robots.txt", "https://localhost:8089/robots.txt"), 8089);
+        SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("user agent");
+        String url = "http://localhost:8089/page.txt";
+        UrlDatum urlDatum = new UrlDatum(url);
+        String key = keyGen.getGroupingKey(urlDatum);
 
         Assert.assertEquals(GroupingKey.DEFERRED_GROUPING_KEY, key);
     }
     
     @Test
     public void testAllowedDisallowedURL() throws Exception {
-        HttpServer server = startServer(new ResourcesResponseHandler("/groupingkeytests"), 8089);
+        _server = startServer(new ResourcesResponseHandler("/groupingkeytests"), 8089);
         SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("testAgent");
         
         String url = "http://localhost:8089/allowed/page.html";
@@ -52,32 +72,28 @@ public class SimpleGroupingKeyGeneratorTest extends SimulationWebServer {
         urlDatum = new UrlDatum(url);
         key = keyGen.getGroupingKey(urlDatum);
         Assert.assertEquals(GroupingKey.BLOCKED_GROUPING_KEY, key);
-
-        server.stop();
     }
     
     @Test
     public void testUsingPLD() throws Exception {
-        HttpServer server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_NOT_FOUND), 8089);
+        _server = startServer(new FixedStatusResponseHandler(HttpStatus.SC_NOT_FOUND), 8089);
         SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("user agent", null, true);
         String url = "http://localhost:8089/page.html";
         UrlDatum urlDatum = new UrlDatum(url);
         String key = keyGen.getGroupingKey(urlDatum);
-        server.stop();
 
-        Assert.assertEquals("localhost-30000", key);
+        Assert.assertEquals("localhost-unset", key);
     }
     
     @Test
     public void testTwitterWithPLD() throws Exception {
-        HttpServer server = startServer(new ResourcesResponseHandler("/twitter"), 8089);
+        _server = startServer(new ResourcesResponseHandler("/twitter"), 8089);
         SimpleGroupingKeyGenerator keyGen = new SimpleGroupingKeyGenerator("testAgent", null, true);
         
         String url = "http://localhost:8089/";
         UrlDatum urlDatum = new UrlDatum(url);
         String key = keyGen.getGroupingKey(urlDatum);
-        server.stop();
 
-        Assert.assertEquals("localhost-30000", key);
+        Assert.assertEquals("localhost-unset", key);
     }
 }
