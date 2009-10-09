9e1b10454e9ccb03fdfad90fcf2b3d93cd1d2ecc
Ken Krugler <kkrugler@transpac.com>
2009-10-09 09:50:26 -0700

    Added serialization test.

---
+++ src/test/java/bixo/exceptions/BaseFetchExceptionTest.java
@@ -0,0 +1,48 @@
+package bixo.exceptions;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import bixo.datum.UrlStatus;
+
+
+public class BaseFetchExceptionTest {
+	
+	@SuppressWarnings("serial")
+	@Test
+	public void testSerialization() throws IOException {
+		BaseFetchException e = new BaseFetchException("url", "msg") {
+
+			@Override
+			public UrlStatus mapToUrlStatus() {
+				return UrlStatus.UNFETCHED;
+			}
+		};
+		
+		ByteArrayOutputStream backingStore = new ByteArrayOutputStream();
+		DataOutputStream output = new DataOutputStream(backingStore);
+		e.writeBaseFields(output);
+		
+		BaseFetchException e2 = new BaseFetchException() {
+
+			@Override
+			public UrlStatus mapToUrlStatus() {
+				return UrlStatus.UNFETCHED;
+			}
+		};
+		
+		ByteArrayInputStream sourceBytes = new ByteArrayInputStream(backingStore.toByteArray());
+		DataInputStream input = new DataInputStream(sourceBytes);
+		e2.readBaseFields(input);
+		
+		Assert.assertEquals("msg", e2.getMessage());
+		Assert.assertEquals("url", e2.getUrl());
+	}
+}
