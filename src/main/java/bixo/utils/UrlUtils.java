2d0e085437f325b2dcd0e0a637a208b7c3c8f163
Ken Krugler <kkrugler@transpac.com>
2009-10-15 15:06:48 -0700

    Fixed bug where check for JavaScript: and such was case-sensitive.
    Fixed up formatting.

--- src/main/java/bixo/utils/UrlUtils.java
+++ src/main/java/bixo/utils/UrlUtils.java
@@ -2,36 +2,44 @@ package bixo.utils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
 public class UrlUtils {
-	private static final Logger LOGGER = Logger.getLogger(UrlUtils.class);
-	
-	public static String makeUrl(URL baseUrl, String relativeUrl) throws MalformedURLException {
-		// Peel off cases of URLs that aren't actually URLs, or at least don't have protocols
-		// that the Java URL class knows about.
-		if (relativeUrl.startsWith("javascript:") || relativeUrl.startsWith("mailto:") || relativeUrl.startsWith("about:")) {
-			return relativeUrl;
-		}
-		
-		// We need to handle one special case, where the relativeUrl is just
-		// a query string (like "?pid=1"), and the baseUrl doesn't end with
-		// a '/'. In that case, the URL class removes the last portion of
-		// the path, which we don't want.
-		
-		try {
-			if (!relativeUrl.startsWith("?") || (baseUrl.getPath().length() == 0) || baseUrl.getPath().endsWith("/")) {
-				return new URL(baseUrl, relativeUrl).toExternalForm();
-			} else {
-				// for the <file> param, use the base path (which won't include the query string) plus
-				// the relative query string.
-				return new URL(baseUrl.getProtocol(), baseUrl.getHost(), baseUrl.getPort(), baseUrl.getPath() + relativeUrl).toExternalForm();
-			}
-		} catch (MalformedURLException e) {
-			// we can get things like "mail:xxx" (versus mailto:) in href attributes.
-			LOGGER.warn("Invalid relativeUrl parameter: " + relativeUrl);
-			return relativeUrl;
-		}
-	}
+    private static final Logger LOGGER = Logger.getLogger(UrlUtils.class);
+
+    private static final Pattern IGNORED_PROTOCOL_PATTERN = Pattern.compile("(?i)^(javascript|mailto|about):");
+    
+    public static String makeUrl(URL baseUrl, String relativeUrl) throws MalformedURLException {
+        // Peel off cases of URLs that aren't actually URLs, or at least don't have protocols
+        // that the Java URL class knows about.
+        if (IGNORED_PROTOCOL_PATTERN.matcher(relativeUrl).find()) {
+            return relativeUrl;
+        }
+
+        // We need to handle one special case, where the relativeUrl is just
+        // a query string (like "?pid=1"), and the baseUrl doesn't end with
+        // a '/'. In that case, the URL class removes the last portion of
+        // the path, which we don't want.
+
+        try {
+            if (!relativeUrl.startsWith("?") || (baseUrl.getPath().length() == 0)
+                            || baseUrl.getPath().endsWith("/")) {
+                return new URL(baseUrl, relativeUrl).toExternalForm();
+            } else {
+                // for the <file> param, use the base path (which won't include
+                // the query string) plus
+                // the relative query string.
+                return new URL(baseUrl.getProtocol(), baseUrl.getHost(), baseUrl.getPort(), baseUrl
+                                .getPath()
+                                + relativeUrl).toExternalForm();
+            }
+        } catch (MalformedURLException e) {
+            // we can get things like "mail:xxx" (versus mailto:) in href
+            // attributes.
+            LOGGER.warn("Invalid relativeUrl parameter: " + relativeUrl);
+            return relativeUrl;
+        }
+    }
 }
--- src/test/java/bixo/utils/UrlUtilsTest.java
+++ src/test/java/bixo/utils/UrlUtilsTest.java
@@ -6,80 +6,90 @@ import java.net.URL;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class UrlUtilsTest {
-	
-	@Test
-	public void testQueryParamReplacingQueryParam() throws MalformedURLException {
-		URL baseUrl = new URL("http://domain.com?p=1");
-		String derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=2");
-		Assert.assertEquals("http://domain.com/?pid=2", derivedUrl);
-	}
-	
-	@Test
-	public void testQueryParamCase() throws MalformedURLException {
-		URL baseUrl = new URL("http://domain.com");
-		String derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
-		Assert.assertEquals("http://domain.com/?pid=1", derivedUrl);
-	}
 
-	@Test
-	public void testLeadingSlash() throws MalformedURLException {
-		URL baseUrl;
-		
-		baseUrl = new URL("http://domain.com/path/to/file");
-		String derivedUrl = UrlUtils.makeUrl(baseUrl, "/file.html");
-		Assert.assertEquals("http://domain.com/file.html", derivedUrl);
-	}
-	
-	@Test
-	public void testLeadingDotSlash() throws MalformedURLException {
-		URL baseUrl;
+    @Test
+    public void testQueryParamReplacingQueryParam() throws MalformedURLException {
+        URL baseUrl = new URL("http://domain.com?p=1");
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=2");
+        Assert.assertEquals("http://domain.com/?pid=2", derivedUrl);
+    }
 
-		baseUrl = new URL("http://domain.com/path/");
-		String derivedUrl = UrlUtils.makeUrl(baseUrl, "./file.html");
-		Assert.assertEquals("http://domain.com/path/file.html", derivedUrl);
-	}
-	
-	@Test
-	public void testRawFile() throws MalformedURLException {
-		URL baseUrl;
-		String derivedUrl;
+    @Test
+    public void testQueryParamCase() throws MalformedURLException {
+        URL baseUrl = new URL("http://domain.com");
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
+        Assert.assertEquals("http://domain.com/?pid=1", derivedUrl);
+    }
 
-		baseUrl = new URL("http://domain.com/path/");
-		derivedUrl = UrlUtils.makeUrl(baseUrl, "file.html");
-		Assert.assertEquals("http://domain.com/path/file.html", derivedUrl);
-	}
-	
-	@Test
-	public void testRawQuery() throws MalformedURLException {
-		URL baseUrl;
-		String derivedUrl;
+    @Test
+    public void testLeadingSlash() throws MalformedURLException {
+        URL baseUrl;
 
-		// See http://www.communities.hp.com/securitysoftware/blogs/jeff/archive/2007/12/19/RFC-1808-vs-2396-vs-3986_3A00_-Browsers-vs.-programing-languages.aspx
-		// Also http://www.ietf.org/rfc/rfc3986.txt
-		// Also http://issues.apache.org/jira/browse/NUTCH-566
-		// Also http://issues.apache.org/jira/browse/NUTCH-436
-		baseUrl = new URL("http://domain.com/path/");
-		derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
-		Assert.assertEquals("http://domain.com/path/?pid=1", derivedUrl);
-		
-		baseUrl = new URL("http://domain.com/file");
-		derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
-		Assert.assertEquals("http://domain.com/file?pid=1", derivedUrl);
+        baseUrl = new URL("http://domain.com/path/to/file");
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, "/file.html");
+        Assert.assertEquals("http://domain.com/file.html", derivedUrl);
+    }
 
-		baseUrl = new URL("http://domain.com/path/d;p?q#f");
-		derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
-		Assert.assertEquals("http://domain.com/path/d;p?pid=1", derivedUrl);
-	}
-	
-	@Test
-	public void testAbsoluteUrl() throws MalformedURLException {
-		URL baseUrl;
-		
-		baseUrl = new URL("http://domain.com/path/to/file");
-		String derivedUrl = UrlUtils.makeUrl(baseUrl, "http://domain2.com/newpath");
-		Assert.assertEquals("http://domain2.com/newpath", derivedUrl);
-	}
+    @Test
+    public void testJavascript() throws MalformedURLException {
+        URL baseUrl;
+
+        baseUrl = new URL("http://domain.com/");
+        String relativeUrl = "JavaScript:GoURL(dowURL, '')";
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, relativeUrl);
+        Assert.assertEquals(relativeUrl, derivedUrl);
+    }
+    
+    @Test
+    public void testLeadingDotSlash() throws MalformedURLException {
+        URL baseUrl;
+
+        baseUrl = new URL("http://domain.com/path/");
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, "./file.html");
+        Assert.assertEquals("http://domain.com/path/file.html", derivedUrl);
+    }
+
+    @Test
+    public void testRawFile() throws MalformedURLException {
+        URL baseUrl;
+        String derivedUrl;
+
+        baseUrl = new URL("http://domain.com/path/");
+        derivedUrl = UrlUtils.makeUrl(baseUrl, "file.html");
+        Assert.assertEquals("http://domain.com/path/file.html", derivedUrl);
+    }
+
+    @Test
+    public void testRawQuery() throws MalformedURLException {
+        URL baseUrl;
+        String derivedUrl;
+
+        // See
+        // http://www.communities.hp.com/securitysoftware/blogs/jeff/archive/2007/12/19/RFC-1808-vs-2396-vs-3986_3A00_-Browsers-vs.-programing-languages.aspx
+        // Also http://www.ietf.org/rfc/rfc3986.txt
+        // Also http://issues.apache.org/jira/browse/NUTCH-566
+        // Also http://issues.apache.org/jira/browse/NUTCH-436
+        baseUrl = new URL("http://domain.com/path/");
+        derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
+        Assert.assertEquals("http://domain.com/path/?pid=1", derivedUrl);
+
+        baseUrl = new URL("http://domain.com/file");
+        derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
+        Assert.assertEquals("http://domain.com/file?pid=1", derivedUrl);
+
+        baseUrl = new URL("http://domain.com/path/d;p?q#f");
+        derivedUrl = UrlUtils.makeUrl(baseUrl, "?pid=1");
+        Assert.assertEquals("http://domain.com/path/d;p?pid=1", derivedUrl);
+    }
+
+    @Test
+    public void testAbsoluteUrl() throws MalformedURLException {
+        URL baseUrl;
+
+        baseUrl = new URL("http://domain.com/path/to/file");
+        String derivedUrl = UrlUtils.makeUrl(baseUrl, "http://domain2.com/newpath");
+        Assert.assertEquals("http://domain2.com/newpath", derivedUrl);
+    }
 
 }
