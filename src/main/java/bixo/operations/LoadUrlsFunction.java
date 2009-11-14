386a09d895678015621d59bdefc5f64e17ebb412
Ken Krugler <kkrugler@transpac.com>
2009-11-13 16:03:48 -0800

    First cut at simple URL loader that handles text files.
    Needs unit tests, and should be expanded to include regex option for
    parsing more complex input files.

---
+++ src/main/java/bixo/operations/LoadUrlsFunction.java
@@ -0,0 +1,43 @@
+package bixo.operations;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+
+import bixo.cascading.NullContext;
+import bixo.datum.BaseDatum;
+import bixo.datum.UrlDatum;
+import bixo.datum.UrlStatus;
+import cascading.flow.FlowProcess;
+import cascading.operation.BaseOperation;
+import cascading.operation.Function;
+import cascading.operation.FunctionCall;
+import cascading.tuple.Fields;
+
+@SuppressWarnings("serial")
+public class LoadUrlsFunction extends BaseOperation<NullContext> implements Function<NullContext> {
+	private static final Logger LOGGER = Logger.getLogger(LoadUrlsFunction.class);
+	
+	public LoadUrlsFunction(Fields metadataFields) {
+        super(UrlDatum.FIELDS.append(metadataFields));
+	}
+
+	@Override
+	public void operate(FlowProcess process, FunctionCall<NullContext> funcCall) {
+        String url = funcCall.getArguments().getString("line").trim();
+        if ((url.length() == 0) || (url.startsWith("#"))) {
+        	return;
+        }
+        
+    	try {
+        	// Validate the URL
+            new URL(url);
+            UrlDatum urlDatum = new UrlDatum(url, 0, 0, UrlStatus.UNFETCHED, BaseDatum.EMPTY_METADATA_MAP);
+            funcCall.getOutputCollector().add(urlDatum.toTuple());
+        } catch (MalformedURLException e) {
+            LOGGER.error("Invalid URL in input data file: " + url);
+        }
+	}
+}
+
