9f722f24404235cd2b18be9443f925d88b11d1f9
Ken Krugler <kkrugler@transpac.com>
2009-11-17 13:22:18 -0800

    Support optional crawler version number.

--- src/main/java/bixo/config/UserAgent.java
+++ src/main/java/bixo/config/UserAgent.java
@@ -10,26 +10,32 @@ public class UserAgent implements Serializable {
 	private final String _emailAddress;
 	private final String _webAddress;
 	private final String _browserVersion;
-	
-	public UserAgent(String agentName, String emailAddress, String webAddress,
-			String browserVersion) {
-		_agentName = agentName;
-		_emailAddress = emailAddress;
-		_webAddress = webAddress;
-		_browserVersion = browserVersion;
-	}
+	private final String _crawlerVersion;
 	
 	public UserAgent(String agentName, String emailAddress, String webAddress) {
 		this(agentName, emailAddress, webAddress, DEFAULT_BROWSER_VERSION);
 	}
 
+	public UserAgent(String agentName, String emailAddress, String webAddress, String browserVersion) {
+		this(agentName, emailAddress, webAddress, browserVersion, null);
+	}
+	
+	public UserAgent(String agentName, String emailAddress, String webAddress,
+			String browserVersion, String crawlerVersion) {
+		_agentName = agentName;
+		_emailAddress = emailAddress;
+		_webAddress = webAddress;
+		_browserVersion = browserVersion;
+		_crawlerVersion = (crawlerVersion == null ? "" : "/" + crawlerVersion);
+	}
+	
 	public String getAgentName() {
 		return _agentName;
 	}
 
 	public String getUserAgentString() {
-		// Mozilla/5.0 (compatible; mycrawler; +http://www.mydomain.com; mycrawler@mydomain.com)
-		return String.format("%s (compatible; %s; +%s; %s)",
-				_browserVersion, getAgentName(), _webAddress, _emailAddress);
+		// Mozilla/5.0 (compatible; mycrawler/1.0; +http://www.mydomain.com; mycrawler@mydomain.com)
+		return String.format("%s (compatible; %s%s; +%s; %s)",
+				_browserVersion, getAgentName(), _crawlerVersion, _webAddress, _emailAddress);
 	}
 }
