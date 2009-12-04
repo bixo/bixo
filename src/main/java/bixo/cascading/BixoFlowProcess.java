3b1dc4c821c5bafde8dec46e5f3a7a410eff6afe
Ken Krugler <kkrugler@transpac.com>
2009-12-03 17:20:18 -0800

    Comment out call to get Reporter.getCounter, as that doesn't exist in Hadoop 0.18.3. This particular code wasn't working in EC2 anyway, so not a big loss.

--- src/main/java/bixo/cascading/BixoFlowProcess.java
+++ src/main/java/bixo/cascading/BixoFlowProcess.java
@@ -21,11 +21,14 @@ import cascading.tuple.TupleEntryIterator;
 @SuppressWarnings("unchecked")
 public class BixoFlowProcess extends FlowProcess {
     private static final Logger LOGGER = Logger.getLogger(BixoFlowProcess.class);
-    
-    // TODO KKr - extend HadoopFlowProces, use Reporter.NULL for reporter, etc. would
-    // be safer than relying on never casting this to a HadoopFlowProcess when in local
+
+    // TODO KKr - extend HadoopFlowProces, use Reporter.NULL for reporter, etc.
+    // would
+    // be safer than relying on never casting this to a HadoopFlowProcess when
+    // in local
     // mode, which is fragile. Though better might be for FlowProcess to support
-    // an isLocal() call, and a getReporter() call, where it returns null for getReporter
+    // an isLocal() call, and a getReporter() call, where it returns null for
+    // getReporter
     // when running local or if it's not a HadoopFlowProcess.
     private class FakeFlowProcess extends FlowProcess {
 
@@ -35,10 +38,12 @@ public class BixoFlowProcess extends FlowProcess {
         }
 
         @Override
-        public void increment(Enum counter, int amount) { }
+        public void increment(Enum counter, int amount) {
+        }
 
         @Override
-        public void keepAlive() { }
+        public void keepAlive() {
+        }
 
         @Override
         public TupleEntryIterator openTapForRead(Tap tap) throws IOException {
@@ -50,14 +55,14 @@ public class BixoFlowProcess extends FlowProcess {
             return null;
         }
     }
-    
+
     private class HadoopFlowReporter implements IFlowReporter {
         private Reporter _reporter;
-        
+
         public HadoopFlowReporter(Reporter reporter) {
             _reporter = reporter;
         }
-        
+
         @Override
         public void setStatus(Level level, String msg) {
             if ((_reporter != null) && level.isGreaterOrEqual(Level.INFO)) {
@@ -73,72 +78,64 @@ public class BixoFlowProcess extends FlowProcess {
             }
         }
     }
-    
+
     private FlowProcess _baseProcess;
     private boolean _isLocal;
     private List<IFlowReporter> _reporters;
     private Map<Enum, AtomicInteger> _localCounters;
-    
-    
+
     public BixoFlowProcess(FlowProcess baseProcess, IFlowReporter reporter) {
         init(baseProcess, reporter);
     }
-    
 
     public BixoFlowProcess(HadoopFlowProcess baseProcess) {
         IFlowReporter reporter = new HadoopFlowReporter(baseProcess.getReporter());
         init(baseProcess, reporter);
     }
-    
-    
+
     /**
-     * A no-argument constructor for use during testing, when we don't have a real
-     * Cascading FlowProcess to use.
+     * A no-argument constructor for use during testing, when we don't have a
+     * real Cascading FlowProcess to use.
      */
     public BixoFlowProcess() {
         init(new FakeFlowProcess(), new LoggingFlowReporter());
     }
-    
-    
+
     private void init(FlowProcess baseProcess, IFlowReporter reporter) {
         _baseProcess = baseProcess;
-        _isLocal = !(baseProcess instanceof HadoopFlowProcess) || 
-            ((HadoopFlowProcess)baseProcess).getJobConf().get("mapred.job.tracker").equalsIgnoreCase("local");
-        
+        _isLocal = !(baseProcess instanceof HadoopFlowProcess)
+                        || ((HadoopFlowProcess) baseProcess).getJobConf().get("mapred.job.tracker")
+                                        .equalsIgnoreCase("local");
+
         _localCounters = new HashMap<Enum, AtomicInteger>();
         _reporters = new ArrayList<IFlowReporter>();
         addReporter(reporter);
     }
-    
-    
+
     public void addReporter(IFlowReporter reporter) {
         _reporters.add(reporter);
     }
-    
-    
+
     public void setStatus(String msg) {
         setStatus(Level.INFO, msg);
     }
-    
-    
+
     public void setStatus(String msg, Throwable t) {
         for (IFlowReporter reporter : _reporters) {
             reporter.setStatus(msg, t);
         }
     }
-    
-    
+
     public void setStatus(Level level, String msg) {
         for (IFlowReporter reporter : _reporters) {
             reporter.setStatus(level, msg);
         }
     }
-    
-    
+
     @Override
     public void increment(Enum counter, int amount) {
         _baseProcess.increment(counter, amount);
-        
+
         // TODO KKr - decide if I really want to track stuff locally
         if (true || _isLocal) {
             synchronized (_localCounters) {
@@ -151,18 +148,16 @@ public class BixoFlowProcess extends FlowProcess {
             int newValue = curCount.addAndGet(amount);
 
             if (LOGGER.isTraceEnabled()) {
-                LOGGER.trace("Cascading counter: " + counter + (amount > 0 ? " + " : " - ") + Math.abs(amount) + " = " + newValue);
+                LOGGER.trace("Cascading counter: " + counter + (amount > 0 ? " + " : " - ")
+                                + Math.abs(amount) + " = " + newValue);
             }
         }
     }
 
-    
     public void decrement(Enum counter, int amount) {
-        // TODO KKr - figure out if negative values work for Hadoop counters
         increment(counter, -amount);
     }
-    
-    
+
     public int getCounter(Enum counter) {
         // TODO KKr - figure out if I want to use my local counter here
         if (true || _isLocal) {
@@ -173,14 +168,22 @@ public class BixoFlowProcess extends FlowProcess {
                 return 0;
             }
         } else {
-            // TODO KKr - verify that this is the right way to get a counter value, especially the part of
-            // mapping from the Enum to the group/name pair needed for reporter.getCounter().
-            Reporter reporter = ((HadoopFlowProcess)_baseProcess).getReporter();
-            
-            // TODO KKr - on EC2, it looks like counter.getDeclaringClass() returns null, as I get a NPE here
-            // and I've verified that reporter is not null. But looking at the Enum source, I don't see how
-            // that could be unless counter.getClass().getSuperclass() returns null.
-            Counter hadoopCounter = reporter.getCounter(counter.getDeclaringClass().getName(), counter.toString());
+            // TODO KKr - verify that this is the right way to get a counter
+            // value, especially the part of
+            // mapping from the Enum to the group/name pair needed for
+            // reporter.getCounter().
+            Reporter reporter = ((HadoopFlowProcess) _baseProcess).getReporter();
+
+            // TODO KKr - on EC2, it looks like counter.getDeclaringClass()
+            // returns null, as I get a NPE here
+            // and I've verified that reporter is not null. But looking at the
+            // Enum source, I don't see how
+            // that could be unless counter.getClass().getSuperclass() returns
+            // null.
+            Counter hadoopCounter = null; // TODO KKr - getCounter() doesn't
+                                          // exist with Bixo 0.18.3
+                                          // reporter.getCounter(counter.getDeclaringClass().getName(),
+                                          // counter.toString());
             if (hadoopCounter != null) {
                 return (int)hadoopCounter.getCounter();
             } else {
@@ -188,40 +191,36 @@ public class BixoFlowProcess extends FlowProcess {
             }
         }
     }
-    
-    
+
     /**
      * If we're running in local mode, log current counter values.
      */
     public void dumpCounters() {
         if (_isLocal) {
             for (Enum theEnum : _localCounters.keySet()) {
-                LOGGER.info(String.format("Cascading counter: %s = %d", theEnum, _localCounters.get(theEnum).get()));
+                LOGGER.info(String.format("Cascading counter: %s = %d", theEnum, _localCounters
+                                .get(theEnum).get()));
             }
         }
-        
+
         // FUTURE KKr - also dump Hadoop counters to Logger?
     }
-    
-    
+
     @Override
     public Object getProperty(String key) {
         return _baseProcess.getProperty(key);
     }
 
-    
     @Override
     public void keepAlive() {
         _baseProcess.keepAlive();
     }
 
-    
     @Override
     public TupleEntryIterator openTapForRead(Tap tap) throws IOException {
         return _baseProcess.openTapForRead(tap);
     }
 
-    
     @Override
     public TupleEntryCollector openTapForWrite(Tap tap) throws IOException {
         return _baseProcess.openTapForWrite(tap);
