6e1ea579fb7b2dd43bb98d9d1567f592d091bce0
Ken Krugler <kkrugler@transpac.com>
2009-12-16 17:30:19 -0800

    Add support for parsed (Tika) metadata

--- src/main/java/bixo/datum/ParsedDatum.java
+++ src/main/java/bixo/datum/ParsedDatum.java
@@ -1,6 +1,9 @@
 package bixo.datum;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
@@ -12,9 +15,10 @@ public class ParsedDatum extends BaseDatum {
     private final String _language;
     private final String _title;
     private final Outlink[] _outLinks;
+    private final Map<String, String> _parsedMeta;
     
     @SuppressWarnings("unchecked")
-    public ParsedDatum(String url, String parsedText, String language, String title, Outlink[] outLinks, Map<String, Comparable> metaData) {
+    public ParsedDatum(String url, String parsedText, String language, String title, Outlink[] outLinks, Map<String, String> parsedMeta, Map<String, Comparable> metaData) {
         super(metaData);
         
         _url = url;
@@ -22,6 +26,7 @@ public class ParsedDatum extends BaseDatum {
         _language = language;
         _title = title;
         _outLinks = outLinks;
+        _parsedMeta = parsedMeta;
     }
 
     public String getUrl() {
@@ -44,6 +49,10 @@ public class ParsedDatum extends BaseDatum {
         return _outLinks;
     }
     
+    public Map<String, String> getParsedMeta() {
+        return _parsedMeta;
+    }
+    
     // ======================================================================================
     // Below here is all Cascading-specific implementation
     // ======================================================================================
@@ -54,8 +63,9 @@ public class ParsedDatum extends BaseDatum {
     public static final String LANGUAGE_FIELD = fieldName(ParsedDatum.class, "language");
     public static final String TITLE_FIELD = fieldName(ParsedDatum.class, "title");
     public static final String OUTLINKS_FIELD = fieldName(ParsedDatum.class, "outLinks");
+    public static final String PARSED_META_FIELD = fieldName(ParsedDatum.class, "parsedMeta");
 
-    public static final Fields FIELDS = new Fields(URL_FIELD, PARSED_TEXT_FIELD, LANGUAGE_FIELD, TITLE_FIELD, OUTLINKS_FIELD);
+    public static final Fields FIELDS = new Fields(URL_FIELD, PARSED_TEXT_FIELD, LANGUAGE_FIELD, TITLE_FIELD, OUTLINKS_FIELD, PARSED_META_FIELD);
 
     public ParsedDatum(Tuple tuple, Fields metaDataFields) {
         super(tuple, metaDataFields);
@@ -65,7 +75,8 @@ public class ParsedDatum extends BaseDatum {
         _parsedText = entry.getString(PARSED_TEXT_FIELD);
         _language = entry.getString(LANGUAGE_FIELD);
         _title = entry.getString(TITLE_FIELD);
-        _outLinks = convertFromTuple((Tuple)entry.get(OUTLINKS_FIELD));
+        _outLinks = convertTupleToOutlinks((Tuple)entry.get(OUTLINKS_FIELD));
+        _parsedMeta = convertTupleToMap((Tuple)entry.get(PARSED_META_FIELD));
     }
 
     @Override
@@ -76,10 +87,10 @@ public class ParsedDatum extends BaseDatum {
     @SuppressWarnings("unchecked")
     @Override
     protected Comparable[] getStandardValues() {
-        return new Comparable[] { _url, _parsedText, _language, _title, convertToTuple(_outLinks) };
+        return new Comparable[] { _url, _parsedText, _language, _title, convertOutlinksToTuple(_outLinks), convertMapToTuple(_parsedMeta) };
     }
 
-    private Tuple convertToTuple(Outlink[] outLinks) {
+    private Tuple convertOutlinksToTuple(Outlink[] outLinks) {
         Tuple tuple = new Tuple();
         for (Outlink outlink : outLinks) {
             tuple.add(outlink.getToUrl());
@@ -88,13 +99,37 @@ public class ParsedDatum extends BaseDatum {
         return tuple;
     }
 
-    private Outlink[] convertFromTuple(Tuple tuple) {
+    private Outlink[] convertTupleToOutlinks(Tuple tuple) {
         int numOutlinks = tuple.size() / 2;
         Outlink[] result = new Outlink[numOutlinks];
         
         for (int i = 0; i < numOutlinks; i++) {
             int tupleOffset = i * 2;
             result[i] = new Outlink(tuple.getString(tupleOffset), tuple.getString(tupleOffset + 1));
+        }
+        return result;
+    }
+    
+    private Tuple convertMapToTuple(Map<String, String> map) {
+        Tuple result = new Tuple();
+        if (map != null) {
+            for (Entry<String, String> entry : map.entrySet()) {
+                result.add(entry.getKey());
+                result.add(entry.getValue());
+            }
+        }
+        
+        return result;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private Map<String, String> convertTupleToMap(Tuple tuple) {
+        Map<String, String> result = new HashMap<String, String>();
+        Iterator<Comparable> iter = tuple.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            String value = (String)iter.next();
+            result.put(key, value);
         }
         return result;
     }
