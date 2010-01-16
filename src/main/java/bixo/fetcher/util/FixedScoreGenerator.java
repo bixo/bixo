a668f9bfb5509232de9ef3379f1bb469c42c77ad
Ken Krugler <kkrugler@transpac.com>
2010-01-15 16:39:51 -0800

    Added support for specifying default score.

--- src/main/java/bixo/fetcher/util/FixedScoreGenerator.java
+++ src/main/java/bixo/fetcher/util/FixedScoreGenerator.java
@@ -3,7 +3,13 @@ package bixo.fetcher.util;
 @SuppressWarnings("serial")
 public class FixedScoreGenerator extends ScoreGenerator {
 
+    public static final double DEFAULT_SCORE = 1.0;
+    
     private double _score;
+    
+    public FixedScoreGenerator() {
+        this(DEFAULT_SCORE);
+    }
     
     public FixedScoreGenerator(double score) {
         super();
