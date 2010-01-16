f66a9a379468c45c2c347a7af4d10735d185cfe0
Ken Krugler <kkrugler@transpac.com>
2010-01-15 16:41:23 -0800

    Improve support for loading URLs
    - Add rejected status for counter
    - Support max # of URLs to import

--- src/main/java/bixo/hadoop/ImportCounters.java
+++ src/main/java/bixo/hadoop/ImportCounters.java
@@ -1,7 +1,7 @@
 package bixo.hadoop;
 
 public enum ImportCounters {
-    URLS_FILTERED,      // URLs rejected
+    URLS_FILTERED,      // URLs removed due to max URL constraint
+    URLS_REJECTED,      // URLS rejected because they were invalid
     URLS_ACCEPTED,      // URLs accepted
-
 }
