4f258867ceb8b9867b5c1f9ffa38fdc48c3d0311
vivek <vivek@OliveToo.local>
2010-09-08 15:20:04 -0700

    Fixed up SimpleStatusTool to work with the output of the SimpleCrawlTool.

--- src/main/java/bixo/tools/SimpleStatusTool.java
+++ src/main/java/bixo/tools/SimpleStatusTool.java
@@ -1,5 +1,7 @@
 package bixo.tools;
 
+import java.io.IOException;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
@@ -8,14 +10,12 @@ import org.apache.log4j.Logger;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
-import bixo.datum.UrlDatum;
 import bixo.datum.UrlStatus;
 import bixo.utils.CrawlDirUtils;
 import cascading.scheme.SequenceFile;
 import cascading.scheme.TextLine;
 import cascading.tap.Hfs;
 import cascading.tap.Tap;
-import cascading.tuple.Fields;
 import cascading.tuple.TupleEntry;
 import cascading.tuple.TupleEntryIterator;
 
@@ -27,7 +27,69 @@ public class SimpleStatusTool {
         System.exit(-1);
     }
 
-	public static void main(String[] args) {
+	private static void processStatus(JobConf conf, Path curDirPath) throws IOException {
+        Path statusPath = new Path(curDirPath, SimpleCrawlConfig.STATUS_SUBDIR_NAME);
+        Tap statusTap = new Hfs(new TextLine(), statusPath.toUri().toString());
+        
+        TupleEntryIterator iter = statusTap.openForRead(conf);
+        
+        UrlStatus[] statusValues = UrlStatus.values();
+        int[] statusCounts = new int[statusValues.length];
+        int totalEntries = 0;
+        while (iter.hasNext()) {
+            TupleEntry entry = iter.next();
+            totalEntries += 1;
+    
+            // URL_FIELD, STATUS_FIELD, HEADERS_FIELD, EXCEPTION_FIELD, STATUS_TIME_FIELD, HOST_ADDRESS_FIELD
+            String statusLine = entry.getString("line");
+            String[] pieces = statusLine.split("\t");
+            UrlStatus status = UrlStatus.valueOf(pieces[1]);
+            statusCounts[status.ordinal()] += 1;
+        }
+        
+        
+        for (int i = 0; i < statusCounts.length; i++) {
+        	if (statusCounts[i] != 0) {
+        		LOGGER.info(String.format("Status %s: %d", statusValues[i].toString(), statusCounts[i]));
+        	}
+        }
+        LOGGER.info("Total status: " + totalEntries);
+        LOGGER.info("");
+    }
+
+    private static void processCrawlDb(JobConf conf, Path curDirPath, boolean exportDb) throws IOException {
+        TupleEntryIterator iter;
+        int totalEntries;
+        Path crawlDbPath = new Path(curDirPath, SimpleCrawlConfig.CRAWLDB_SUBDIR_NAME);
+        Tap crawldbTap = new Hfs(new SequenceFile(CrawlDbDatum.FIELDS), crawlDbPath.toUri().toString());
+        iter = crawldbTap.openForRead(conf);
+        totalEntries = 0;
+        int fetchedUrls = 0;
+        int unfetchedUrls = 0;
+        
+        while (iter.hasNext()) {
+            TupleEntry entry = iter.next();
+            totalEntries += 1;
+            
+            CrawlDbDatum datum = new CrawlDbDatum(entry.getTuple());
+            if (exportDb) {
+                LOGGER.info(datum.toString());
+            }
+            if (datum.getLastFetched() == 0) {
+            	unfetchedUrls += 1;
+            } else {
+            	fetchedUrls += 1;
+            }
+        }
+        if (!exportDb) {
+            LOGGER.info(String.format("%d fetched URLs", fetchedUrls));
+            LOGGER.info(String.format("%d unfetched URLs", unfetchedUrls));
+            LOGGER.info("Total URLs: " + totalEntries);
+            LOGGER.info("");
+        }
+    }
+
+    public static void main(String[] args) {
         SimpleStatusToolOptions options = new SimpleStatusToolOptions();
         CmdLineParser parser = new CmdLineParser(options);
         
@@ -53,74 +115,32 @@ public class SimpleStatusTool {
         	// Skip Hadoop/Cascading DEBUG messages.
         	Logger.getRootLogger().setLevel(Level.INFO);
         	
-        	int prevLoop = -1;
-        	Path curDirPath = null;
-        	while ((curDirPath = CrawlDirUtils.findNextLoopDir(fs, crawlDirPath, prevLoop)) != null) {
-        		String curDirName = curDirPath.toUri().toString();
-        		LOGGER.info("");
-        		LOGGER.info("================================================================");
-        		LOGGER.info("Processing " + curDirName);
-        		LOGGER.info("================================================================");
-        		
-        		int curLoop = CrawlDirUtils.extractLoopNumber(curDirPath);
-        		if (curLoop != prevLoop + 1) {
-        			LOGGER.warn(String.format("Missing directories between %d and %d", prevLoop, curLoop));
-        		}
-        		
-        		prevLoop = curLoop;
-        		
-        		// Process the content, status and urls in curPath
-                Tap statusTap = new Hfs(new TextLine(), curDirName + "/status");
-                
-                TupleEntryIterator iter = statusTap.openForRead(conf);
-                
-                UrlStatus[] statusValues = UrlStatus.values();
-                int[] statusCounts = new int[statusValues.length];
-                int totalEntries = 0;
-                while (iter.hasNext()) {
-                    TupleEntry entry = iter.next();
-                    totalEntries += 1;
-
-                    // http://www.mcafee.com/apps/campaigns/survey.asp?mktg=vRn1KQ2oV7cJXdVj0Xudyks4CQ%3d%3d	SKIPPED_BY_SCORE	null	null	1255201363035
-                    // <url>  <status>  <headers>  <exception>  <time>
-                    String statusLine = entry.getString("line");
-                    String[] pieces = statusLine.split("\t");
-                    UrlStatus status = UrlStatus.valueOf(pieces[1]);
-                    statusCounts[status.ordinal()] += 1;
-                }
-                
-                
-                for (int i = 0; i < statusCounts.length; i++) {
-                	if (statusCounts[i] != 0) {
-                		LOGGER.info(String.format("Status %s: %d", statusValues[i].toString(), statusCounts[i]));
-                	}
-                }
-                LOGGER.info("Total status: " + totalEntries);
-                LOGGER.info("");
-                
-                Fields metaDataFields = new Fields();
-                Tap urlTap = new Hfs(new SequenceFile(UrlDatum.FIELDS), curDirName + "/urls");
-                iter = urlTap.openForRead(conf);
-                totalEntries = 0;
-                int fetchedUrls = 0;
-                int unfetchedUrls = 0;
-                
-                while (iter.hasNext()) {
-                    TupleEntry entry = iter.next();
-                    totalEntries += 1;
+        	boolean exportDb = options.isExportDb();
+        	if (exportDb) {
+        	    Path latestCrawlDirPath = CrawlDirUtils.findLatestLoopDir(fs, crawlDirPath);
+        	    processCrawlDb(conf, latestCrawlDirPath, exportDb);
+        	} else {
+            	int prevLoop = -1;
+            	Path curDirPath = null;
+            	while ((curDirPath = CrawlDirUtils.findNextLoopDir(fs, crawlDirPath, prevLoop)) != null) {
+            		String curDirName = curDirPath.toUri().toString();
+            		LOGGER.info("");
+            		LOGGER.info("================================================================");
+            		LOGGER.info("Processing " + curDirName);
+            		LOGGER.info("================================================================");
+            		
+            		int curLoop = CrawlDirUtils.extractLoopNumber(curDirPath);
+            		if (curLoop != prevLoop + 1) {
+            			LOGGER.warn(String.format("Missing directories between %d and %d", prevLoop, curLoop));
+            		}
+            		
+            		prevLoop = curLoop;
+            		
+            		// Process the status and crawldb in curPath
+            		processStatus(conf, curDirPath);
+                    processCrawlDb(conf, curDirPath, exportDb);
                     
-                    UrlDatum datum = new UrlDatum(entry.getTuple(), metaDataFields);
-                    if (datum.getLastFetched() == 0) {
-                    	unfetchedUrls += 1;
-                    } else {
-                    	fetchedUrls += 1;
-                    }
-                }
-                
-                LOGGER.info(String.format("%d fetched URLs", fetchedUrls));
-                LOGGER.info(String.format("%d unfetched URLs", unfetchedUrls));
-                LOGGER.info("Total URLs: " + totalEntries);
-                LOGGER.info("");
+            	}
         	}
         } catch (Throwable t) {
         	LOGGER.error("Exception running tool", t);
