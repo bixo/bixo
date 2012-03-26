d4bde12d2813da5a389d95c924d374ed2b4fb2e6
vivek <vivek@OliveToo.local>
2012-03-26 14:34:24 -0700

    Some more files that needed to have the license boilerplate updated.

--- src/main/java/bixo/utils/StringUtils.java
+++ src/main/java/bixo/utils/StringUtils.java
@@ -18,23 +18,6 @@ package bixo.utils;
 
 import java.util.ArrayList;
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 public class StringUtils {
 	private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5',
 			'6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
