6a6230ec12ec9d6bdf95dbcddc8f31532746c40f
Ken Krugler <ken@transpac.com>
2012-03-28 09:48:44 -0700

    Strip out namespace from XML

--- src/main/java/bixo/parser/DOMParser.java
+++ src/main/java/bixo/parser/DOMParser.java
@@ -18,10 +18,16 @@ package bixo.parser;
 
 import java.io.InputStream;
 
+import javax.xml.XMLConstants;
+
 import org.ccil.cowan.tagsoup.Parser;
 import org.dom4j.Document;
 import org.dom4j.io.SAXReader;
 import org.hsqldb.lib.StringInputStream;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+import org.xml.sax.helpers.XMLFilterImpl;
 
 import bixo.datum.ParsedDatum;
 import bixo.utils.IoUtils;
@@ -38,6 +44,51 @@ import com.bixolabs.cascading.NullContext;
 @SuppressWarnings("serial")
 public abstract class DOMParser extends BaseOperation<NullContext> implements Function<NullContext> {
 
+    /**
+     * Strip out XML namespace, so that XPath can be easily used to extract elements.
+     *
+     */
+    private static class DowngradeXmlFilter extends XMLFilterImpl {
+        
+        @Override
+        public void startElement(String uri, String localName, String name, Attributes atts)
+                throws SAXException {
+            String lower = localName.toLowerCase();
+
+            AttributesImpl attributes = new AttributesImpl();
+            for (int i = 0; i < atts.getLength(); i++) {
+                String local = atts.getLocalName(i);
+                String qname = atts.getQName(i);
+                if (!XMLConstants.NULL_NS_URI.equals(atts.getURI(i).length())
+                        && !local.equals(XMLConstants.XMLNS_ATTRIBUTE)
+                        && !qname.startsWith(XMLConstants.XMLNS_ATTRIBUTE + ":")) {
+                    attributes.addAttribute(
+                            atts.getURI(i), local, qname,
+                            atts.getType(i), atts.getValue(i));
+                }
+            }
+
+            super.startElement(XMLConstants.NULL_NS_URI, lower, lower, attributes);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String name)
+                throws SAXException {
+            String lower = localName.toLowerCase();
+            super.endElement(XMLConstants.NULL_NS_URI, lower, lower);
+        }
+
+        @Override
+        public void startPrefixMapping(String prefix, String uri) {
+        }
+
+        @Override
+        public void endPrefixMapping(String prefix) {
+        }
+    }
+    
+    
+
     private transient SAXReader _reader = null;
     private transient ParsedDatum _input;
     
@@ -50,6 +101,7 @@ public abstract class DOMParser extends BaseOperation<NullContext> implements Fu
         super.prepare(process, opCall);
         
         _reader = new SAXReader(new Parser());
+        _reader.setXMLFilter(new DowngradeXmlFilter());
         _reader.setEncoding("UTF-8");
         _input = new ParsedDatum();
     }
